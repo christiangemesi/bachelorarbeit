83381c63d14fef786489d806973974ed8b4e7273
Nick <nick.gaston.koch@bluewin.ch>
2018-10-01 11:12:42 +0200

    Fixed another typo

--- public/js/user/themebox-data.js
+++ public/js/user/themebox-data.js
@@ -448,7 +448,7 @@ $(document).ready(function () {
 
             createEvent(formatCalendarDate($("#start-date").val()), formatCalendarEndDate($("#end-date").val()));
         }else{
-            errorHandling("Ihre Auswahl steht in konflikt mit einem anderen Bestelltermin", "#error-calendar-message-box");
+            errorHandling("Ihre Auswahl steht in Konflikt mit einem anderen Bestelltermin", "#error-calendar-message-box");
             $('#carousel-right').prop('disabled', true);
         }
     }

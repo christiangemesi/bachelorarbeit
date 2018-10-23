bb3280195468e210439768fcf9500ec0d604534b
Nick <nick.gaston.koch@bluewin.ch>
2018-10-23 17:08:18 +0200

    added print view

---
+++ resources/views/admin/print.blade.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+<div class="modal-body">
+    <div class="row">
+        <form id="order-edit-form" autocomplete="off">
+            <div class="col-md-12 thekre-row">
+                <input type="hidden" value="" id="order-id"/>
+                <table id="print-order">
+
+                </table>
+            </div>
+        </form>
+    </div>
+</div>

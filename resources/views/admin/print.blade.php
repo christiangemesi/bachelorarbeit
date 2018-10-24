b2faf33df6c8d4ac00523385261b062456352587
Nick <nick.gaston.koch@bluewin.ch>
2018-10-24 09:20:14 +0200

    id: order-edit-form is already in use

--- resources/views/admin/print.blade.php
+++ resources/views/admin/print.blade.php
@@ -2,13 +2,11 @@
 
 <div class="modal-body">
     <div class="row">
-        <form id="order-edit-form" autocomplete="off">
             <div class="col-md-12 thekre-row">
                 <input type="hidden" value="" id="order-id"/>
                 <table id="print-order">
 
                 </table>
             </div>
-        </form>
     </div>
 </div>

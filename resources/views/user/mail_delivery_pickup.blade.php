1044cc08d273482dc1abf55f491925f7680cb9d5
Nick <nick.gaston.koch@bluewin.ch>
2018-10-29 16:22:12 +0100

    fixed wrong url in mail

--- resources/views/user/mail_delivery_pickup.blade.php
+++ resources/views/user/mail_delivery_pickup.blade.php
@@ -16,7 +16,7 @@
     Wir bitten Sie, diese am <b>{{ $startdate }} </b>abzuholen.
     Falls Sie die Kiste nicht am vereinbarten Termin abholen können, wird die Kiste zurück ins Regal gestellt und steht dann den anderen Benutzenden wieder zur Verfügung.
     <br><br>
-    Ihre Bestellung können Sie unter <a href="http://http://server1121.cs.technik.fhnw.ch/user/loginForm">Bestellverwaltung</a> mit den folgenden Daten bearbeiten:
+    Ihre Bestellung können Sie unter <a href="http://server1121.cs.technik.fhnw.ch/user/loginForm">Bestellverwaltung</a> mit den folgenden Daten bearbeiten:
     <br>
 
     Nachname Bestellperson:  <b>{{ $receiver_name }}</b><br>

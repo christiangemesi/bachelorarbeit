b9f6fc1f655cd811dbbc86dd5312c8092b035da0
Nick <nick.gaston.koch@bluewin.ch>
2018-10-31 17:17:09 +0100

    fixed change password

--- resources/views/admin/indexChangePassword.blade.php
+++ resources/views/admin/indexChangePassword.blade.php
@@ -26,8 +26,7 @@
             {{ session()->get('success-message') }}
         </div>
     @endif
-
-
+    
     <div class="col-md-12 admin-panel  data-table-thekre">
         <h1 class="admin-header">Passwort ändern</h1>
         <div class="panel panel-default no-border  margin-top-less" id="table-content">
@@ -38,19 +37,18 @@
                             <form action="updatePassword" method="post" id="password-change-form" autocomplete="off">
                                 <div class="form-group">
                                     <label class="float-left" for="password-now">Aktuelles Passwort:</label>
-                                    <input type="password" name="password-now" class="form-control" id="password-now" required/>
+                                    <input type="password" name="password-now" class="form-control" id="password-now" onchange="checkPassword()" required/>
                                 </div>
                                 <div class="form-group">
                                     <label class="float-left" for="password">Neues Passwort:</label>
-                                    <input type="password" name="password" class="form-control" id="password" required/>
+                                    <input type="password" name="password" class="form-control" id="password" onchange="checkPassword()" required/>
                                 </div>
                                 <div class="form-group">
                                     <label class="float-left" for="confirm_password">Passwort erneut eingeben:</label>
-                                    <input type="password" class="form-control" name="confirm_password" id="confirm_password" required/>
+                                    <input type="password" class="form-control" name="confirm_password" id="confirm_password" onchange="checkPassword()" required/>
                                 </div>
                                 <div class="form-group">
-                                    <input type="submit" class="btn btn-primary float-right" onclick="checkPassword()" value="Speichern"/>
-                                    <!--<button class="btn btn-primary float-right" onclick="checkPassword()">Speichern</button>-->
+                                    <input type="submit" class="btn btn-primary float-right" value="Speichern"/>
                                 </div>
                              </form>
                         </div>
@@ -59,6 +57,5 @@
             </div>
         </div>
     </div>
-
 
 @endsection

7c8624d0f0ad0636dc77261a079edd03c7e2c076
Nick <nick.gaston.koch@bluewin.ch>
2019-01-09 15:47:36 +0100

    fixed links again

--- resources/views/layouts/nav_user.blade.php
+++ resources/views/layouts/nav_user.blade.php
@@ -4,17 +4,17 @@
         <ul class="sidebar-nav">
             <li class="sidebar-brand">
                 <a href="https://www.fhnw.ch">
-                    <img src="/images/fhnw_logo_w_claim_de.svg" id="fhnw-logo" alt="fhnw logo">
+                    <img src="../images/fhnw_logo_w_claim_de.svg" id="fhnw-logo" alt="fhnw logo">
                 </a>
             </li>
             <li class="sidebar-nav-text">
-                <a href="/user"><div class="nav-text"> Bestellformular</div></a>
+                <a href="../user"><div class="nav-text"> Bestellformular</div></a>
             </li>
             <li class="sidebar-nav-text">
-                <a href="/user/loginForm"><div class="nav-text"> Bestellverwaltung</div></a>
+                <a href="../user/loginForm"><div class="nav-text"> Bestellverwaltung</div></a>
             </li>
             <li class="sidebar-nav-text site-switch">
-                <a href="/admin"><div class="nav-text"> Administrator Login</div></a>
+                <a href="../admin"><div class="nav-text"> Administrator Login</div></a>
             </li>
         </ul>
     </div>

2be60de25c8d08b7c495afc2d1e452c98f3e5862
Nick <nick.gaston.koch@bluewin.ch>
2019-01-14 09:55:27 +0100

    try to to set path relative to .env variable

--- resources/views/layouts/nav_user.blade.php
+++ resources/views/layouts/nav_user.blade.php
@@ -4,14 +4,14 @@
         <ul class="sidebar-nav">
             <li class="sidebar-brand">
                 <a href="https://www.fhnw.ch">
-                    <img src="../images/fhnw_logo_w_claim_de.svg" id="fhnw-logo" alt="fhnw logo">
+                    <img src="{{ env('APP_URL') }}/images/fhnw_logo_w_claim_de.svg" id="fhnw-logo" alt="fhnw logo">
                 </a>
             </li>
             <li class="sidebar-nav-text">
-                <a href="/themenkisten/user"><div class="nav-text"> Bestellformular</div></a>
+                <a href="{{ env('APP_URL') }}/user"><div class="nav-text"> Bestellformular</div></a>
             </li>
             <li class="sidebar-nav-text">
-                <a href="/themenkisten/user/loginForm"><div class="nav-text"> Bestellverwaltung</div></a>
+                <a href="{{ env('APP_URL') }}/user/loginForm"><div class="nav-text"> Bestellverwaltung</div></a>
             </li>
         </ul>
     </div>

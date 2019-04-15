1f887e60c9a483c3b98c4fd01964ff5b9c69b66c
Nick <nick.gaston.koch@bluewin.ch>
2019-04-15 14:01:04 +0200

    ???

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -227,8 +227,7 @@ class UserController extends Controller
         }
 
         Mail::send($view, $mail_data, function ($message) use ($mail_data) {
-            $message->to($mail_data['receiver_mail'], $mail_data['receiver_name'] . " " . $mail_data['receiver_surname']);
-            $message->bcc("bibliothek.windisch@fhnw.ch", "Bestellung ThekRe")->subject('Bestellbestätigung Themenkiste');
+            $message->to($mail_data['receiver_mail'], $mail_data['receiver_name'] . " " . $mail_data['receiver_surname'])->bcc('bibliothek.windisch@fhnw.ch', 'Bestellung ThekRe')->subject('Bestellbestätigung Themenkiste');
         });
     }
 

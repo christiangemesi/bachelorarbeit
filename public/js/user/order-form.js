2a8344cabbceed87888e48893d36e65eea78346b
maurice.meier <maurice.meier@fhnw.ch>
2021-08-13 13:08:57 +0200

    Bibliotheksausweis ist Kein Pflichtfeld Mehr

--- public/js/user/order-form.js
+++ public/js/user/order-form.js
@@ -160,28 +160,30 @@ function phoneValidate() {
 }
 
 function nebisValidate() {
-    if (nebisusernumber.value == "") {
-        document.getElementById("nebisInputStatus").innerHTML = "Nebisnummer wird benötigt!";
-        document.getElementById("nebisInputStatus").style.display = "block";
-        nebisusernumber.parentNode.className = "form-group has-error has-feedback";
-        document.getElementById("nebisIcon").className = "glyphicon glyphicon-remove form-control-feedback";
-        checkValidation(0);
-        return false;
-    } else
-    if (!notEmpty(nebisusernumber.value)) {
-        document.getElementById("nebisInputStatus").innerHTML = "Falsches Nebisnummer Format!";
-        document.getElementById("nebisInputStatus").style.display = "block";
-        nebisusernumber.parentNode.className = "form-group has-warning has-feedback";
-        document.getElementById("nebisIcon").className = "glyphicon glyphicon-warning-sign form-control-feedback";
-        checkValidation(0);
-        return false;
-    } else {
-        document.getElementById("nebisInputStatus").style.display = "none";
-        nebisusernumber.parentNode.className = "form-group has-success has-feedback";
-        document.getElementById("nebisIcon").className = "glyphicon glyphicon-ok form-control-feedback";
-        checkValidation(0);
-        return true;
-    }
+    // if (nebisusernumber.value == "") {
+    //     document.getElementById("nebisInputStatus").innerHTML = "Nebisnummer wird benötigt!";
+    //     document.getElementById("nebisInputStatus").style.display = "block";
+    //     nebisusernumber.parentNode.className = "form-group has-error has-feedback";
+    //     document.getElementById("nebisIcon").className = "glyphicon glyphicon-remove form-control-feedback";
+    //     checkValidation(0);
+    //     return false;
+    // } else
+    // if (!notEmpty(nebisusernumber.value)) {
+    //     document.getElementById("nebisInputStatus").innerHTML = "Falsches Nebisnummer Format!";
+    //     document.getElementById("nebisInputStatus").style.display = "block";
+    //     nebisusernumber.parentNode.className = "form-group has-warning has-feedback";
+    //     document.getElementById("nebisIcon").className = "glyphicon glyphicon-warning-sign form-control-feedback";
+    //     checkValidation(0);
+    //     return false;
+    // } else {
+    //     document.getElementById("nebisInputStatus").style.display = "none";
+    //     nebisusernumber.parentNode.className = "form-group has-success has-feedback";
+    //     document.getElementById("nebisIcon").className = "glyphicon glyphicon-ok form-control-feedback";
+    //     checkValidation(0);
+    //     return true;
+    // }
+    checkValidation(0);
+    return true;
 }
 
 /**

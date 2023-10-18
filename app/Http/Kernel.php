937de207d34f19069f79302f71aed89d8068173b
Christian Gémesi <christian.gemesi@students.fhnw.ch>
2023-10-18 17:06:46 +0200

    upgraded and documented laravel 6 -> laravel 7

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends HttpKernel
         \ThekRe\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \ThekRe\Http\Middleware\TrustProxies::class,
-        \Barryvdh\Cors\HandleCors::class,
+        \Fruitcake\Cors\HandleCors::class,
     ];
 
     /**

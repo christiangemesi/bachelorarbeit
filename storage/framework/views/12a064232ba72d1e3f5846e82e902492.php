d3b2c1c422cf2080105ba7264e4e5f32a075fa8b
Christian Gémesi <christian.gemesi@students.fhnw.ch>
2023-10-26 19:08:20 +0200

    laravel docker kinda works, with exception and unable to get info, but at least it mounts, i think :)

---
+++ storage/framework/views/12a064232ba72d1e3f5846e82e902492.php
@@ -0,0 +1,22 @@
+
+<?php $__env->startSection('title', 'ThekRe - Poweruser'); ?>
+
+<?php $__env->startSection('content'); ?>
+
+    <?php echo $__env->make('layouts.nav_background', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    <div class="alert alert-danger" id="login-error-message-box">
+        Login Fehlgeschlagen.
+    </div>
+
+    <script src="<?php echo e(asset('/js/poweruser/login.js')); ?>"></script>
+
+    <div class="col-md-12 admin-panel">
+        <h1 class="admin-header">Poweruser Login</h1>
+        <div class="form-group">
+            <label for="password">Passwort:</label>
+            <input type="password" class="form-control" id="poweruser-password" name="password"/>
+        </div>
+        <button type="submit" id="login-button" class="btn btn-success btn-lg">Login</button>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\leihmich\thek-re-2\resources\views/poweruser/login_form.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/48c92efa40926a28e86938fb9dbc6f12.php
@@ -0,0 +1,22 @@
+
+<?php $__env->startSection('title', 'ThekRe - Admin'); ?>
+
+<?php $__env->startSection('content'); ?>
+
+    <?php echo $__env->make('layouts.nav_background', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    <div class="alert alert-danger" id="login-error-message-box">
+        Login Fehlgeschlagen.
+    </div>
+
+    <script src="<?php echo e(asset('/js/admin/login.js')); ?>"></script>
+
+    <div class="col-md-12 admin-panel">
+        <h1 class="admin-header">Administrator Login</h1>
+        <div class="form-group">
+            <label for="password">Passwort:</label>
+            <input type="password" class="form-control" id="admin-password" name="password"/>
+        </div>
+        <button type="submit" id="login-button" class="btn btn-success btn-lg">Login</button>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\leihmich\thek-re-2\resources\views/admin/login_form.blade.php ENDPATH**/ ?>

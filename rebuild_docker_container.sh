75adcb17c9b03c01925c3eb9f9c20d7afbd1c5a7
Christian Gémesi <christian.gemesi@students.fhnw.ch>
2024-02-09 16:13:44 +0100

    :

--- rebuild_docker_container.sh
+++ rebuild_docker_container.sh
@@ -7,14 +7,14 @@ git fetch origin master
 if ! git diff --quiet origin/master; then
     echo "Changes detected. Rebuilding Docker container..."
 
-    # Stop the current Docker container if it's running
-    sudo docker compose down || { echo "Error: Unable to stop the Docker container"; exit 1; }
-
     # Pull the latest changes from the remote repository
-    error_msg=$(sudo git pull origin master 2>&1) || { echo "Error: Unable to pull the latest changes: $error_msg"; exit 1; }
+    sudo git pull origin master || { echo "Error: Unable to pull the latest changes"; exit 1; }
 
     # Build the Docker image
     sudo docker compose build || { echo "Error: Unable to build the Docker image"; exit 1; }
+
+    # Stop the current Docker container if it's running
+    sudo docker compose down || { echo "Error: Unable to stop the Docker container"; exit 1; }
 
     # Start the Docker container in the background
     sudo docker compose up -d || { echo "Error: Unable to start the Docker container"; exit 1; }

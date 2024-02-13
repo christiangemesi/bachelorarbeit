0829791c696f14124be22e46dd3fd95cf4c5dd5a
Christian Gémesi <christian.gemesi@students.fhnw.ch>
2024-02-13 11:22:03 +0100

    logging more

--- rebuild_docker_container.sh
+++ rebuild_docker_container.sh
@@ -11,19 +11,37 @@ if ! git diff --quiet HEAD origin/master; then
     echo "$current_date - Changes detected. Rebuilding Docker container..."
 
     # Pull the latest changes from the remote repository
-    git pull origin master || { echo "$current_date - Error: Unable to pull the latest changes"; exit 1; }
+    git pull origin master 2>&1 || {
+        error_message=$(git pull origin master 2>&1)
+        echo "$current_date - Error: $error_message";
+        exit 1;
+    }
 
     # Build the Docker image
-    docker compose build || { echo "$current_date - Error: Unable to build the Docker image"; exit 1; }
+    docker compose build 2>&1 || {
+        error_message=$(docker compose build 2>&1)
+        echo "$current_date - Error: $error_message";
+        exit 1;
 
     # Stop the current Docker container if it's running
-    docker compose down || { echo "$current_date - Error: Unable to stop the Docker container"; exit 1; }
+    docker compose down 2>&1 || {
+        error_message=$(docker compose down 2>&1)
+        echo "$current_date - Error: $error_message";
+        exit 1;
+    }
 
     # Start the Docker container in the background
-    docker compose up -d || { echo "$current_date - Error: Unable to start the Docker container"; exit 1; }
+    docker compose up -d 2>&1 || {
+        error_message=$(docker compose up -d 2>&1)
+        echo "$current_date - Error: $error_message";
+        exit 1;
 
-    # remove dangling images so that the disk space is not consumed
-    docker rmi $(docker images --filter "dangling=true" -q --no-trunc) || { echo "$current_date - Error: Unable to remove dangling images"; exit 1; }
+    # Remove dangling images so that the disk space is not consumed
+    docker rmi $(docker images --filter "dangling=true" -q --no-trunc) 2>&1 || {
+        error_message=$(docker rmi $(docker images --filter "dangling=true" -q --no-trunc) 2>&1)
+        echo "$current_date - Error: $error_message";
+        exit 1;
+    }
 
     echo "$current_date - Docker container rebuilt and started successfully."
 else

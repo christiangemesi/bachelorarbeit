bdbe1120bf9cec7b43a27fd051c0263559c934a9
Christian Gémesi (Laptop) <christian.gemesi@students.fhnw.ch>
2024-02-28 16:57:50 +0100

    fixed order success page

--- resources/views/user/order_success.blade.php
+++ resources/views/user/order_success.blade.php
@@ -4,29 +4,29 @@
 @section('content')
     @include('layouts.nav_user')
 
-        <div id="page-content-wrapper">
-            <div class="col-md-12 main-Panel">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h1>Bestellung erfolgreich abgeschlossen!</h1>
-                    </div>
-                </div>
-                <br><br>
-                <div class="row">
-                    <div class="col-md-1">
-                        <span class="glyphicon glyphicon-ok-circle glyphicon-order-success"></span>
-                    </div>
+    <div id="col-md-12 admin-panel">
+        <div class="col-md-12 main-Panel">
 
-                    <div class="col-md-10">
-                        <p class="order_feeback_text">
-                            Vielen Dank für Ihre Bestellung. <br>
-                            Sie erhalten in Kürze eine <b>E-Mail</b> mit den Informationen zu Ihrer Bestellung.
-                            Es besteht die Möglichkeit, dass die Bestätigungs E-Mail in Ihrem Spam Ordner landet.</p>
-                    </div>
-                </div><br>
+            <div class="col-md-12">
+                <h1>Bestellung erfolgreich abgeschlossen!</h1>
+            </div>
+
+            <br><br>
+
+            <div class="col-md-12">
+                <span class="glyphicon glyphicon-ok-circle glyphicon-order-success"></span>
+            </div>
+
+            <div class="col-md-12">
+                <p class="order_feeback_text">
+                    Vielen Dank für Ihre Bestellung. <br>
+                    Sie erhalten in Kürze eine <b>E-Mail</b> mit den Informationen zu Ihrer Bestellung.
+                    Es besteht die Möglichkeit, dass die Bestätigungs E-Mail in Ihrem Spam Ordner landet.</p>
                 <a href="../">
                     <button type="button" class="btn btn-primary">neues Ausleihobjekt bestellen</button>
                 </a>
             </div>
+            <br>
         </div>
+    </div>
 @endsection
